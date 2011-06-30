b418ce602df54ee0265d496a2a57943b99694ff5
Damien Pobel <damien@pobel.fr>
2011-06-30 22:16:58 +0000

    Updated fre-FR translation file and added untranslated translation file

--- extension/autostatus/translations/untranslated/translation.ts
+++ extension/autostatus/translations/untranslated/translation.ts
@@ -29,7 +29,7 @@
     </message>
     <message>
         <source>Social network</source>
-        <translation type="unfinished">Réseau social</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Status message</source>
@@ -87,54 +87,6 @@
 <context>
     <name>design/admin/workflow/eventtype/edit</name>
     <message>
-        <source>Class attribute to use for status</source>
-        <translation>Attribut à utiliser pour le status</translation>
-    </message>
-    <message>
-        <source>Content class</source>
-        <translation>Classe de contenu</translation>
-    </message>
-    <message>
-        <source>No suitable attribute in the selected content class</source>
-        <translation>Aucun attribut n&apos;est utilisable dans cette classe de contenu</translation>
-    </message>
-    <message>
-        <source>Choose a content class</source>
-        <translation>Choisissez une classe de contenu</translation>
-    </message>
-    <message>
-        <source>Attribute to use for status message</source>
-        <translation>Attribut à utiliser pour le message de status</translation>
-    </message>
-    <message>
-        <source>Social network</source>
-        <translation>Réseau social</translation>
-    </message>
-    <message>
-        <source>Choose an attribute</source>
-        <translation>Choisissez un attribut</translation>
-    </message>
-    <message>
-        <source>Choose a social network</source>
-        <translation>Choisissez un réseau social</translation>
-    </message>
-    <message>
-        <source>Login</source>
-        <translation>Login</translation>
-    </message>
-    <message>
-        <source>Password</source>
-        <translation>Mot de passe</translation>
-    </message>
-    <message>
-        <source>Defer status update to cronjob</source>
-        <translation>Reporter la mise à jour du status au cronjob</translation>
-    </message>
-    <message>
-        <source>Use cronjob to update status</source>
-        <translation>Utiliser le script cron pour mettre à jour le status</translation>
-    </message>
-    <message>
         <source>Your access has already been granted on %network.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -155,11 +107,51 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No suitable attribute in the selected content class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Send every time</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Account informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Social network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose a social network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Class attribute to use for status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose a content class</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Attribute to use for status message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose an attribute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -176,6 +168,14 @@
     </message>
     <message>
         <source>Available attributes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Defer status update to cronjob</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use cronjob to update status</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -231,23 +231,15 @@
     <name>kernel/workflow/event</name>
     <message>
         <source>Auto status</source>
-        <translation>Auto status</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Invalid content class</source>
-        <translation>Classe de contenu invalide</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Invalid attribute</source>
-        <translation>Attribut invalide</translation>
-    </message>
-    <message>
-        <source>Invalid social network</source>
-        <translation>Réseau social invalide</translation>
-    </message>
-    <message>
-        <source>Login cannot be empty</source>
-        <translation>Login ne peut être vide</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Invalid way of triggering the udpate (none actually)</source>
@@ -255,6 +247,14 @@
     </message>
     <message>
         <source>You need to choose a social network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid social network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login cannot be empty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
